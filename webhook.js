b49f0b71cb61b59d90587446890f13596a66847b
kanatip02 <38550445+kanatip02@users.noreply.github.com>
2020-10-22 21:53:52 +0700

    update

--- webhook.js
+++ webhook.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -8,6 +9,32 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
+
+
+
+const request = require('request');
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: EAAvDZBKkHOuoBAByjZC7HNLEdSwoShq6fGz4CQMJfjCs6fyDoO8p59StxM8VS2B2N3zqOGqhpCUZBRDpnY5MOeIHdbOcDZC6OoKqb7XHNlP33yTj6qpX49z1Wzpt41Fa1yDQdIq5JOtKBW7sE23Yv15aTzieTy65YCaEMwFfJAZDZD},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+
 /* For Facebook Validation */
 app.get('/', (req, res) => {
     if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
@@ -16,6 +43,8 @@ app.get('/', (req, res) => {
       res.status(403).end();
     }
   });
+  
+
   
   /* Handling all messenges */
   app.post('/webhook', (req, res) => {
@@ -31,3 +60,5 @@ app.get('/', (req, res) => {
       res.status(200).end();
     }
   });
+ 
+ 
