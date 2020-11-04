865844f84e5dd039209408ca9aec107ea36fc66f
kanatip02 <38550445+kanatip02@users.noreply.github.com>
2020-11-05 01:32:00 +0700

    get userame complete

--- webhook.js
+++ webhook.js
@@ -14,20 +14,16 @@ const request = require('request');
 function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
-
-
-  
- 
-
+  let username = event.message.text;
 
   request({
     
-    url: "https://graph.facebook.com/v8.0/me/messages?access_token=EAAvDZBKkHOuoBAA8ZArDU956J3kXChuml6TduQ1pcgnuDCnZCp3ZCJEWSXiQ8ZAgZCSnnPXIQAgfhXhYyvv9W1EebcnaRc3nZCAxZCVL6NoxJ8Ugo6mt4BcdGZCUbD1yZB1cV2YPM4ejWqvPCm27DqPUCsq78UbWhmcb8kZBcww0kD6zgZDZD",
+    url: "https://graph.facebook.com/v8.0/me/messages?access_token=EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD",
     //qs: {access_token: EAAvDZBKkHOuoBAByjZC7HNLEdSwoShq6fGz4CQMJfjCs6fyDoO8p59StxM8VS2B2N3zqOGqhpCUZBRDpnY5MOeIHdbOcDZC6OoKqb7XHNlP33yTj6qpX49z1Wzpt41Fa1yDQdIq5JOtKBW7sE23Yv15aTzieTy65YCaEMwFfJAZDZD},
     method: 'POST',
     json: {
       recipient: {id: sender},
-      message: {text:"Bot said: " + text}
+      message: {text:"Bot said: " + text }
     }
   }, function (error, response) {
     if (error) {
@@ -52,17 +48,41 @@ app.get('/', (req, res) => {
   
   /* Handling all messenges */
   app.post('/', (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     if (req.body.object === 'page') {
       req.body.entry.forEach((entry) => {
         entry.messaging.forEach((event) => {
           if (event.message && event.message.text) {
-            sendMessage(event);
+            //sendMessage(event);
+            getName(event);
+            //console.log(event.sender.id);
           }
         });
       });
       res.status(200).end();
     }
   });
- 
- 
+
+
+  function getName(event){
+    var sender = event.sender.id
+    let token = "EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD"
+    request({
+      url: "https://graph.facebook.com/v8.0/" + sender,
+      qs: {
+          access_token : token,
+          fields: "first_name"
+      },
+      
+
+      method: "GET",
+
+      json: true,
+      time: true
+    }, 
+    function (error, response, faceUserInfo) {
+      console.log("faceUserInfo", faceUserInfo);
+      let name = faceUserInfo.first_name
+      console.log("Name of User : ", name);
+    });
+  }
