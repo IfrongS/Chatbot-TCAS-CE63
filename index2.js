467a47eb504f87f79f4ab253e1e4419fa5edeb83
kanatip02 <38550445+kanatip02@users.noreply.github.com>
2021-01-22 04:28:30 +0700

    v2

--- index2.js
+++ index2.js
@@ -18,22 +18,57 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
   const request = require('request');
 
-  request({
+  function sendMessage(event) {
+    let sender = event.sender.id;
+    let text = event.message.text;
+    request({  
+      url: "https://graph.facebook.com/v8.0/me/messages?access_token=EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD",
+      //qs: {access_token: EAAvDZBKkHOuoBAByjZC7HNLEdSwoShq6fGz4CQMJfjCs6fyDoO8p59StxM8VS2B2N3zqOGqhpCUZBRDpnY5MOeIHdbOcDZC6OoKqb7XHNlP33yTj6qpX49z1Wzpt41Fa1yDQdIq5JOtKBW7sE23Yv15aTzieTy65YCaEMwFfJAZDZD},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text:"Bot said: Hello " + text }
+      }
+    }, function (error, response) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error body: ', response.body.error);
+      }
+    });
+  }
+
+
+  function getName(event){
+    var sender = event.sender.id
+    let token = "EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD"
     
-    url: "https://graph.facebook.com/v8.0/me/messages?access_token=EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD",
-    //qs: {access_token: EAAvDZBKkHOuoBAByjZC7HNLEdSwoShq6fGz4CQMJfjCs6fyDoO8p59StxM8VS2B2N3zqOGqhpCUZBRDpnY5MOeIHdbOcDZC6OoKqb7XHNlP33yTj6qpX49z1Wzpt41Fa1yDQdIq5JOtKBW7sE23Yv15aTzieTy65YCaEMwFfJAZDZD},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text:"Bot said: Hello " + text }
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error body: ', response.body.error);
-    }
-  });
+    let dateFB = event.timestamp;
+    let new_date = new Date(dateFB*1000);
+    let current_date = new Date();
+
+    request({
+      url: "https://graph.facebook.com/v8.0/" + sender,
+      qs: {
+          access_token : token,
+          fields: "first_name,last_name,profile_pic"
+      },
+      
+
+      method: "GET",
+
+      json: true,
+      time: true
+    }, 
+    function (error, response, body) {
+      console.log("faceUserInfo", body);
+      let name = body.first_name
+      console.log("Name of User : ", name);
+      console.log('Date: ' + current_date );
+     
+      
+    });
+  }
 
  
   function welcome(agent) {
