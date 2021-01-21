ba6ed27c08bc65fdf8b1cbd80b76a3870d0ee5a0
kanatip02 <38550445+kanatip02@users.noreply.github.com>
2021-01-22 04:24:43 +0700

    update

--- index2.js
+++ index2.js
@@ -4,11 +4,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const db = admin.firestore();
-admin.initializeApp();
-/*admin.initializeApp({
+//admin.initializeApp();
+admin.initializeApp({
 		credential : admin.credential.applicationDefault(),
   		databaseURL : 'ws://chatbot-pdbp.firebaseio.com/'
-});*/
+});
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
@@ -16,6 +16,25 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  const request = require('request');
+
+  request({
+    
+    url: "https://graph.facebook.com/v8.0/me/messages?access_token=EAAvDZBKkHOuoBADNmfwWxuZBJJJkZBcqcH2VXtXcfx67gAf5CPocGzoAhASh6whmKscC6AW7IVIbD2AKHfz7ipBijuZBvQ5UciLXUdCSD0vk6nPdPOwvHcjIfeemoMLGdYwZAlIZAoEDgZCBkynlIBxZAu3UwdPSaM5BGWUZACTZB69wZDZD",
+    //qs: {access_token: EAAvDZBKkHOuoBAByjZC7HNLEdSwoShq6fGz4CQMJfjCs6fyDoO8p59StxM8VS2B2N3zqOGqhpCUZBRDpnY5MOeIHdbOcDZC6OoKqb7XHNlP33yTj6qpX49z1Wzpt41Fa1yDQdIq5JOtKBW7sE23Yv15aTzieTy65YCaEMwFfJAZDZD},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text:"Bot said: Hello " + text }
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error body: ', response.body.error);
+    }
+  });
+
  
   function welcome(agent) {
     agent.add(`Welcome to my agent!`);
